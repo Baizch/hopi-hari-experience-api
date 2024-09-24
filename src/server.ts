3b081ff72ac70dd8b193a42f744c13ed780ed37a
baizch <francobaisch00@gmail.com>
2024-09-24 16:09:25 -0300

    fix: hot reload not working

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,26 @@
 import express from 'express';
+import { Router } from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
 
+// Server configuration
 dotenv.config();
-
 connectDB();
-
+const app = express();
+const router = Router();
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Teste');
+// Routes
+router.get('/', async (req, res) => {
+  res.status(200).json({ message: 'Default route' });
 });
+
+router.get('/attractions', async (req, res) => {
+  res.status(200).json({ message: 'Teste' });
+});
+
+app.use('/api', router);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
